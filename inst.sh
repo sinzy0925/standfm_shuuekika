3cf1ed18e7adf6689359f7d455b48b82c6225291
sinzy0925 <84183154+sinzy0925@users.noreply.github.com>
2023-09-26 17:20:50 +0900

    Add files via upload

---
+++ inst.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo [google shell]---
+echo npx playwright install-deps
+npx playwright install-deps
+
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
+echo stand.fm 露骨な表現の修正
+echo
+echo 【注意】 全て半角で、データを貼り付けてください。
+echo node app_shuuekika.js \'の\'が半角か要確認。
+echo 最後の行に\'を忘れてないか要確認。　
+echo \(例\)以下のように貼り付けてください。
+echo
+echo node app_shuuekika.js \'
+echo mail
+echo password
+echo 1
+echo 2
+echo 10
+echo \'
+echo
+
+
