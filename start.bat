1fcbba46d772466d8b4961bbf173a1529997aca4
sinzy0925 <84183154+sinzy0925@users.noreply.github.com>
2023-10-28 19:44:32 +0900

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,13 @@
+rem @echo off
+setlocal EnableDelayedExpansion
+rem set LF=^
+rem 1行目変更不可 node app_gakkyokushinsei_win.js ^  
+rem 2行目^^の間にメルアド記入
+rem 3行目^^の間にパスワード記入
+rem 4行目以降^^の間に作品コード,アーカイブ順,//曲名を記入
+
+node app_gakkyokushinsei_win.js ^
+^mail^
+^pass^
+^N00501449,1,//HOWEVER^
+^096-5892-1,1,//super star 長渕剛^
